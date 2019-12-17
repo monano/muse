f674672f226f6ca54b1d596e5fa57bff0199eecd
monano <mona.gaihab@gmail.com>
2019-12-17 09:30:35 +0300

    pushed the lab

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.create(name: "Rihanna", hometwon: "Barbados", img: "http://mccarthyamp.com/wp-content/uploads/2016/02/23445669273_bfc7c4062b_b.jpg", albums: 6)
+Artist.create(name: "Taylor Swift", hometwon: "Nashville", img: "https://upload.wikimedia.org/wikipedia/en/f/f6/Taylor_Swift_-_1989.png", albums: 8)
+Artist.create(name: "Billy Joel", hometwon: "Long Island", img: "https://images-na.ssl-images-amazon.com/images/I/81RgoBLQOKL._SY355_.jpg", albums: 14)
+Artist.create(name: "Drake", hometwon: "Toronto", img: "http://images.complex.com/complex/image/upload/t_in_content_image/drake-thank-me-later-album-cover_o6ek33.jpg", albums: 4)
+Artist.create(name: "Beyonce", hometwon: "Houston", img: "http://www.fuse.tv/image/571c26a6017704456e00001b/816/545/beyonce-lemonade-album-cover-full.jpg", albums: 6)
+Artist.create(name: "Salem", hometwon: "Bagdad", img: "https://images-na.ssl-images-amazon.com/images/I/81RgoBLQOKL._SY355_.jpg", albums: 3)
+Artist.create(name: "Yauso", hometwon: "Kyma", img: "http://images.complex.com/complex/image/upload/t_in_content_image/drake-thank-me-later-album-cover_o6ek33.jpg", albums: 10)
+Artist.create(name: "Nami", hometwon: "Saitama", img: "http://www.fuse.tv/image/571c26a6017704456e00001b/816/545/beyonce-lemonade-album-cover-full.jpg", albums: 7)
